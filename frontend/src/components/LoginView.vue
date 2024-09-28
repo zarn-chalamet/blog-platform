ebffa04c88487ee06af12879fd7eb33931d33fea
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2024-09-28 15:29:31 +0700

    login and signup frontend

---
+++ frontend/src/components/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Log In</h2>
+    <form @submit.prevent="loginFun">
+        <input type="email" placeholder="email" v-model="email">
+        <input type="password" placeholder="password" v-model="password">
+        <p v-if="error">{{ error }}</p>
+        <button>
+            Login
+        </button>      
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/RegisterView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>SignUp</h2>
+    <form @submit.prevent="signUp">
+      <input type="text" placeholder="display name" v-model="displayName">
+      <input type="email" placeholder="email" v-model="email">
+      <input type="password" placeholder="password" v-model="password">
+      <p v-if="error">{{ error }}</p>
+      <button>
+          Sign up
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
