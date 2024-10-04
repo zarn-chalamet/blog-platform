c36d4fbea44f016ffd6ccf42d5a6cd9015b63031
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2024-10-05 00:15:31 +0700

    add search function

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,46 @@
 <template>
   <div>
-    <div v-for="blog in blogs" :key="blog._id">
-      <SingleBlog :blog="blog"></SingleBlog>
+    <input type="text" placeholder="Search" v-model="search" />
+    <div v-for="blog in filteredBlogs" :key="blog._id">
+      <SingleBlog :blog="blog" />
     </div>
   </div>
 </template>
-<script>
-import SingleBlog from '../components/SingleBlog.vue'
-import { onMounted, ref } from 'vue';
 
+<script>
+import SingleBlog from '../components/SingleBlog.vue';
+import { onMounted, ref, computed } from 'vue';
 import { useBlogStore } from '@/stores/blog';
 
-  export default{
-    components: {
+export default {
+  components: {
     SingleBlog,
-      },
-    setup(){
-      const blogStore = useBlogStore();
-      let blogs = ref(null);
+  },
+  setup() {
+    const blogStore = useBlogStore();
+    const blogs = ref([]);
+    const search = ref('');
 
-      onMounted( async () => {
-        blogs.value = await blogStore.getBlogs();
-      });
+    // Load blogs on mount
+    onMounted(async () => {
+      blogs.value = await blogStore.getBlogs();
+    });
 
+    // Computed property to filter blogs based on search input
+    const filteredBlogs = computed(() => {
+      if (!search.value) {
+        return blogs.value;
+      }
+      return blogs.value.filter((item) =>
+        item.title.toLowerCase().includes(search.value.toLowerCase())
+      );
+    });
 
-      
-      return {blogs};
-    }
-  }
+    return {
+      blogs,
+      search,
+      filteredBlogs,
+    };
+  },
+};
 </script>
